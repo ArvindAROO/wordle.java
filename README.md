b12757fcf96330696c184bf287e7f4816039a346
ArvindAROO <i.arvindkrishna@gmail.com>
2022-02-08 17:46:41 +0530

    initial dump

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# wordle.java
+# Java implementation of the Worlde game
+
+
+
+
+Assumptions: I assumed you use dark mode like a normal person, and colors are implemented for that. This might lead to non-readable output for light mode terminals.
+
+![image](./images/example.png)
+## Guidelines
+
+
+1. Clone this repository
+
+2. Compile the program using the command `javac wordle.java`
+
+3. Run the program using the command `java wordle`
+
+## Credits
+
+* The game Wordle is entirely and purely inspired from the game [Worlde](https://www.powerlanguage.co.uk/wordle/).
+
+* The allowed_word list and possible_word list in `/data` is taken from [3Blue1Brown's repo](https://github.com/3b1b/videos/tree/master/_2022/wordle_data)
+* The solver program algorithm is entirely inspired by the awesome video by Grant Sanderson (3Blue1Brown) on using information theory to solve wordle puzzles - [here](https://www.youtube.com/watch?v=v68zYyaEmEA), just implemented in java instead of python
+
+Note: This is just a very crude implementation of the game, to be used in the automatic solver program in https://github.com/ArvindAROO/Wordle-Solver (repo private? guess it's not complete yet)
---
+++ wordle.java
@@ -0,0 +1,156 @@
+// Java Program to Print Colored Text in Console
+
+// import necessary packages
+import java.io.*;
+import java.nio.file.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+ 
+
+class Colorize{
+    public static void print(String s, String[] colorset){
+
+        final String ANSI_RESET = "\u001B[0m";
+        
+
+        // print each letter with its corresponding color
+        for(int i=0; i<s.length(); i++){
+            System.out.print(colorset[i] + s.charAt(i) + ANSI_RESET);
+        }
+        System.out.println();
+    }
+}
+
+class check{
+    public static int[] test(String actualWord, String guess){
+        final String GREEN = "\u001B[42m";
+        final String YELLOW = "\u001B[43m";
+        final String GREY = ""; //passing nothing will make the text grey
+        
+        String[] sol = {"", "", "", "", ""};
+        int[] score = {-1, -1, -1, -1, -1};
+        // for each letter in guess
+        // if it is in correcte dposition in actual word, put green
+        // if it is in wrong position, put yellow
+        // if its not in actual word, put grey
+        for(int i=0; i<actualWord.length(); i++){
+            if(actualWord.charAt(i) == guess.charAt(i)){
+                sol[i] = GREEN;
+                score[i] = 2;
+            }
+            else if(actualWord.indexOf(guess.charAt(i)) != -1){
+                // actualWord.contains(guess.charAt(i))){
+                sol[i] = YELLOW;
+                score[i] = 1;
+            }
+            else{
+                sol[i] = GREY;
+                score[i] = 0;
+            }
+        }
+        
+        Colorize.print(guess, sol);
+        return score;
+    }
+
+}
+
+class load{
+    public static Object[] loadData()
+        throws IOException
+    {
+        // list that holds strings of a file
+        List<String> reader = new ArrayList<String>();
+        
+        // for some reason, java needs absolute file path, so find current path, and append relative path
+        String filePath = new File("").getAbsolutePath();
+
+        //read files
+        reader = Files.readAllLines(Paths.get(filePath + "/data/allowed_words.txt"));
+        String[] allowedWords = reader.toArray(new String[0]);
+        System.out.println("Number of allowed words: " + allowedWords.length);
+
+        reader = Files.readAllLines(Paths.get(filePath + "/data/possible_words.txt"));
+        String[] possibleWords = reader.toArray(new String[0]);
+        System.out.println("Number of possible words: " + possibleWords.length);
+
+        // for (String eachString : allowedWords) {
+        //     System.out.println(eachString);
+        // }
+        // for (String eachString : possibleWords) {
+        //     System.out.println(eachString);
+        // }
+        return new Object[]{allowedWords, possibleWords};
+
+    }
+}
+
+class play{
+    final String GREEN = "\u001B[42m";
+    final String RED = "\u001B[41m";
+    final String ANSI_RESET = "\u001B[0m";
+        
+    private String[] allowed_words;
+    private String[] possible_words;
+    public void load() throws IOException{
+        Object[] data = load.loadData();
+        this.allowed_words = (String[]) data[0];
+        this.possible_words = (String[]) data[1];
+        
+    }
+    public void game() {
+
+        //randomly choose a word from possible words
+        int num = (int) (Math.random() * this.possible_words.length);
+        String word = this.possible_words[num];
+        for(int attempts = 1; attempts < 7; ){
+            //take a guess
+            System.out.print("Enter a guess: ");
+            String guess = System.console().readLine();
+            //check if guess in allowed words
+            if(Arrays.asList(this.allowed_words).contains(guess)){
+                check.test(word, guess);
+                attempts++;
+
+                if (guess.equals(word)){
+                    System.out.println(GREEN + "You win!" + ANSI_RESET);
+                    return;
+                }
+                
+            }
+            else{
+                System.out.println("Not in allowed words");
+            }
+            System.out.println();
+            
+        }
+        System.out.println("You lose!");
+        System.out.println(RED + "The word was: " + word + ANSI_RESET);
+    }
+
+}
+
+class wordle {
+    
+    final static String GREEN = "\u001B[42m";
+    final static String YELLOW = "\u001B[43m";
+    final static String GREY = ""; //passing nothing will make the text grey
+    
+
+    public static void main(String[] args) throws IOException
+    {
+        // test to check the colors
+        // int[] score = check.test("hello", "world");
+        // for(int i=0; i<score.length; i++){
+        //     System.out.print(score[i] + " ");
+        // }
+        // System.out.println();
+
+        //call the game function
+        play p = new play();
+        p.load();
+        p.game();
+
+    }
+}
